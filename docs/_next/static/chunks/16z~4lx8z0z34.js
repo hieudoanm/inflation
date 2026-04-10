895e663b00142357836cc4478439720947f533ad
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-04-10 10:05:32 +0700

    Update Templates

--- docs/_next/static/chunks/16z~4lx8z0z34.js
+++ docs/_next/static/chunks/16z~4lx8z0z34.js
@@ -2,7 +2,7 @@ __turbopack_load_page_chunks__("/_app", [
   "static/chunks/0i68pprq3qltk.js",
   "static/chunks/0ax5vutq21_yr.js",
   "static/chunks/16v1rv-0yer48.js",
-  "static/chunks/0.qxxewop-9ed.css",
+  "static/chunks/0025qo75dveqq.css",
   "static/chunks/0eki28umy5xci.css",
-  "static/chunks/turbopack-0w~111g1tos._.js"
+  "static/chunks/turbopack-162~pd~ben.pm.js"
 ])
